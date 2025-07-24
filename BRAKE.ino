f0c66a612cbcbc0a52ac7cbda2790d5e9706a583
SelfdriveVanagon <41362285+Lukilink@users.noreply.github.com>
2025-07-24 23:14:53 +0200

    Update BRAKE.ino

--- BRAKE.ino
+++ BRAKE.ino
@@ -33,139 +33,138 @@ boolean releasing_by_OP = false;
 boolean open_solenoid = true;
 long previousMillis;
 
-
 void setup() {
-//________________begin Monitor - only use for debugging
-// Serial.begin(115200);
+  //________________begin Monitor - only use for debugging
+  // Serial.begin(115200);
 
-//________________begin CAN
-CAN.begin(500E3);
-CAN.filter(0x343);
+  //________________begin CAN
+  CAN.begin(500E3);
+  CAN.filter(0x343);
 
-//________________set up pin modes
-pinMode(pressurePin, INPUT);
-pinMode(M_DIR, OUTPUT); // LOW is Left / HIGH is Right
-pinMode(M_PWM, OUTPUT); // 255 is run / LOW is stop
-pinMode(S_DIR, OUTPUT); // LOW is Left / HIGH is Right
-pinMode(S_PWM, OUTPUT); // 255 is run / LOW is stop
-digitalWrite(S_DIR, HIGH); // the direction for the solenoid does not matter
-pinMode(brakelightPin, OUTPUT);
+  //________________set up pin modes
+  pinMode(pressurePin, INPUT);
+  pinMode(M_DIR, OUTPUT); // LOW is Left / HIGH is Right
+  pinMode(M_PWM, OUTPUT); // 255 is run / LOW is stop
+  pinMode(S_DIR, OUTPUT); // LOW is Left / HIGH is Right
+  pinMode(S_PWM, OUTPUT); // 255 is run / LOW is stop
+  digitalWrite(S_DIR, HIGH); // the direction for the solenoid does not matter
+  pinMode(brakelightPin, OUTPUT);
 }
 
 void loop() {
 
-//________________read pressure sensor 
-currentPressure = (analogRead(pressurePin));
+  //________________read pressure sensor 
+  currentPressure = (analogRead(pressurePin));
 
-//________________read ACC_CMD from CANbus
- CAN.parsePacket();
+  //________________read ACCEL_CMD from CANbus (DBC-konform)
+  CAN.parsePacket();
 
- if (CAN.packetId() == 0x343)
-      {
-      uint8_t dat[8];
-      for (int ii = 0; ii <= 7; ii++) {
-        dat[ii]  = (char) CAN.read();
-        }
-        ACC_CMD = ((dat[0] << 8 | dat[1] << 0) * -1); 
-        }
-
-//________________calculating ACC_CMD into ACC_CMD_PERCENT
-if (ACC_CMD >= minACC_CMD) {
-    ACC_CMD1 = ACC_CMD;
-    }
-else {
-    ACC_CMD1 = minACC_CMD;
-    }
-       
-ACC_CMD_PERCENT = ((100/(maxACC_CMD - minACC_CMD)) * (ACC_CMD1 - minACC_CMD));
-
-//________________calculating targetPressure
-targetPressure = (((ACC_CMD_PERCENT / 100) * (maxPressure - minPressure)) + minPressure); // conversion from ACC_CMD_PERCENT % into targetpressure
- 
-//________________press or release the pedal to match targetPressure
-if (abs(currentPressure - targetPressure) >= PERM_ERROR)
+  if (CAN.packetId() == 0x343)
   {
-    open_solenoid = false;
-    if ((currentPressure < targetPressure) && (ACC_CMD_PERCENT > 0))
-        { 
-        analogWrite(M_PWM, 255);  //run Motor
-        digitalWrite(M_DIR, HIGH); //motor driection left | press the pedal
-        releasing_by_OP = false;
-        }    
-    else if (currentPressure > targetPressure)
-        {       
-        analogWrite(M_PWM, 255);   //run Motor
-        digitalWrite(M_DIR, LOW); //motor driection right | release the pedal
-        releasing_by_OP = true;
-        }
-    else if (ACC_CMD_PERCENT == 0)
-        {       
-        analogWrite(M_PWM, 255);   //run Motor
-        digitalWrite(M_DIR, LOW); //motor driection right | release the pedal
-        releasing_by_OP = false;
-        }
+      uint8_t dat[8];
+      for (int ii = 0; ii < 8; ii++) {
+          dat[ii]  = (uint8_t) CAN.read();
+      }
+      // ACCEL_CMD: 16 bit signed, little endian, Startbit 7, DBC: 7|16@0-
+      int16_t raw_accel_cmd = (int16_t)(dat[1] << 8 | dat[0]); // Little Endian!
+      ACC_CMD = raw_accel_cmd * -0.001; // DBC: Faktor 0.001, Vorzeichenumkehr
+  }
+
+  //________________calculating ACC_CMD into ACC_CMD_PERCENT
+  if (ACC_CMD >= minACC_CMD) {
+      ACC_CMD1 = ACC_CMD;
+  }
+  else {
+      ACC_CMD1 = minACC_CMD;
+  }
+        
+  ACC_CMD_PERCENT = ((100/(maxACC_CMD - minACC_CMD)) * (ACC_CMD1 - minACC_CMD));
+
+  //________________calculating targetPressure
+  targetPressure = (((ACC_CMD_PERCENT / 100) * (maxPressure - minPressure)) + minPressure); // conversion from ACC_CMD_PERCENT % into targetpressure
+
+  //________________press or release the pedal to match targetPressure
+  if (abs(currentPressure - targetPressure) >= PERM_ERROR)
+  {
+      open_solenoid = false;
+      if ((currentPressure < targetPressure) && (ACC_CMD_PERCENT > 0))
+      { 
+          analogWrite(M_PWM, 255);  //run Motor
+          digitalWrite(M_DIR, HIGH); //motor driection left | press the pedal
+          releasing_by_OP = false;
+      }    
+      else if (currentPressure > targetPressure)
+      {       
+          analogWrite(M_PWM, 255);   //run Motor
+          digitalWrite(M_DIR, LOW); //motor driection right | release the pedal
+          releasing_by_OP = true;
+      }
+      else if (ACC_CMD_PERCENT == 0)
+      {       
+          analogWrite(M_PWM, 255);   //run Motor
+          digitalWrite(M_DIR, LOW); //motor driection right | release the pedal
+          releasing_by_OP = false;
+      }
   }  
-else 
-    {
-     open_solenoid = false;
-     analogWrite(M_PWM, 0);   //if we match target position, just stay here
-     releasing_by_OP = false;
-    }  
+  else 
+  {
+      open_solenoid = false;
+      analogWrite(M_PWM, 0);   //if we match target position, just stay here
+      releasing_by_OP = false;
+  }  
 
-//________________logic to read if brake is pressed by driver   
-if ((currentPressure >= (targetPressure + brake_pressed_threshold)) && !releasing_by_OP)
-    {
-        BRAKE_PRESSED = true;
-        open_solenoid = true;
-    }
-    
-else {
-        BRAKE_PRESSED = false;
-     }
- 
-//________________operate solenoid
- if (open_solenoid)
-    { 
-    analogWrite(S_PWM, 0);
-    }
- else
-    { 
-    analogWrite(S_PWM, 255);
-    }
+  //________________logic to read if brake is pressed by driver   
+  if ((currentPressure >= (targetPressure + brake_pressed_threshold)) && !releasing_by_OP)
+  {
+      BRAKE_PRESSED = true;
+      open_solenoid = true;
+  }
+  else {
+      BRAKE_PRESSED = false;
+  }
 
- //________________light up brakelights when brake is pressed
-long currentMillis = millis();
-if (currentMillis - previousMillis >= 200)
-    {  
-     if (currentPressure >= (minPressure + brake_light_threshold))
-        {
-         analogWrite(brakelightPin, 255);
-        }
-     else 
-        {
-         analogWrite(brakelightPin, 0);
-        }
-     previousMillis = currentMillis;
-     }
+  //________________operate solenoid
+  if (open_solenoid)
+  { 
+      analogWrite(S_PWM, 0);
+  }
+  else
+  { 
+      analogWrite(S_PWM, 255);
+  }
 
-//________________send CAN BUS messages
-//0x3b7 msg ESP_CONTROL
-  uint8_t dat_3b7[8];
-  dat_3b7[0] = (BRAKE_PRESSED << 5) & 0x20;
-  dat_3b7[1] = 0x0;
-  dat_3b7[2] = 0x0;
-  dat_3b7[3] = 0x0;
-  dat_3b7[4] = 0x0;
-  dat_3b7[5] = 0x0;
-  dat_3b7[6] = 0x0;
-  dat_3b7[7] = 0x08;
+  //________________light up brakelights when brake is pressed
+  long currentMillis = millis();
+  if (currentMillis - previousMillis >= 200)
+  {  
+      if (currentPressure >= (minPressure + brake_light_threshold))
+      {
+          analogWrite(brakelightPin, 255);
+      }
+      else 
+      {
+          analogWrite(brakelightPin, 0);
+      }
+      previousMillis = currentMillis;
+  }
+
+  //________________send CAN BUS messages, DBC-konform (ESP_CONTROL, ID 0x3b7)
+  uint8_t dat_3b7[8] = {0};
+
+  // BRAKE_LIGHTS_ACC auf Bit 18 (Byte 2, Bit 2)
+  if (BRAKE_PRESSED) {
+      dat_3b7[2] |= (1 << 2);
+  }
+
+  // [weitere Signale hier nach Bedarf, z.B. TC_DISABLED, VSC_DISABLED, usw.]
+
   CAN.beginPacket(0x3b7);
   for (int ii = 0; ii < 8; ii++) {
-    CAN.write(dat_3b7[ii]);
+      CAN.write(dat_3b7[ii]);
   }
   CAN.endPacket();
 
-//________________print stuff if you want to DEBUG
+  //________________print stuff if you want to DEBUG
   /*
   Serial.print("ACC_CMD_");
   Serial.print(ACC_CMD);
